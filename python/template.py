e7aab1b48419d5b3317666308c99df5357a57cd1
T4Thor <tech@4thor.com>
2022-10-22 02:10:31 +0200

    Improved python script template with functions, logging and docstrings

--- python/template.py
+++ python/template.py
@@ -1,21 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # Author: Thomas Thormann
-# Last edit:
+"""This is a script description."""
 
-# Imports
-import argparse, sys, subprocess, json, os
+from argparse import ArgumentParser
+import logging
+
+
+def parse_args():
+    """Parse sys.argv[0] and return the Namespace object."""
+    parser = ArgumentParser(description=__doc__)
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+    #                     help='integers for the accumulator')  # positional
+    # parser.add_argument('--foo', required=True, choices=['bar', 'baz'], default='bar', dest='var',
+    #                     help='%(choices)s (default: %(default)s)')  # named
+    parser.add_argument('-v', '--verbose', action='store_true', help='print debug information')
+    return parser.parse_args()
+
+
+def init_logging(verbose: bool):
+    """Init logging class with format and appropriate loglevel."""
+    loglevel = logging.DEBUG if verbose else logging.WARNING
+    logformat = '[%(levelname)s] %(message)s'
+    logging.basicConfig(format=logformat, level=loglevel)
+    # logformat = '%(asctime)s [%(levelname)s] %(message)s'
+    # logfile = "error.log"
+    # logging.basicConfig(format=logformat, level=loglevel, filename=logfile)
+
 
 def main():
-  # Input control
-  parser = argparse.ArgumentParser(description='This is a script.')
-  #parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
-  #parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers (default: find the max)')
-  parser.add_argument('-v', '--verbose', action='store_const', const=True, default=False, help='print debug information')
-  args = parser.parse_args()
-  if args.verbose: print(str(args))
+    """Main function."""
+    args = parse_args()
+    init_logging(args.verbose)
+    logging.debug('Arguments: %s', vars(args))
 
-  # Start of script
-  #print(args.accumulate(args.integers))
+    # Start of script
+    # print('foo', args.var)
+
 
 if __name__ == "__main__":
-  main()
+    main()
